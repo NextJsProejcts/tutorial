839010647cb9c8493b1989c27cefe7d851e81964
Osama-Daja <54779323+Osama-Daja@users.noreply.github.com>
2024-08-25 22:33:20 +0300

    API Project

---
+++ nextjstutorial/API_Project/API_Project/Controllers/TestAPIController.cs
@@ -0,0 +1,43 @@
+﻿using API_Project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestAPIController : Controller
+    {
+        static public List<User> users = new List<User>();
+        public TestAPIController()
+        {
+        }
+
+        [HttpGet]
+        public object getUsers()
+        {
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public object AddUser(User user)
+        {
+            users.Add(user);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{username}")]
+        public object DeleteUser(string username)
+        {
+            var user = users.FirstOrDefault(a => a.Username == username);
+            users.Remove(user);
+            return Ok();
+        }
+
+    }
+}
